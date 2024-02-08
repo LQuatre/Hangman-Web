6ae33b053ff8a803cf0dd23220655f488b574216
mhayet <hayet.mairia@ynov.com>
2024-02-08 15:54:59 +0100

    added difficulty back, forms, and methods GET

--- server.go
+++ server.go
@@ -3,44 +3,60 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 func main() {
-    fs := http.FileServer(http.Dir("assets"))
-    http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	fs := http.FileServer(http.Dir("assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        // Default to serving index.html
-        filePath := "index.html"
-        
-        // If the path is explicitly /hangman.html, serve that file instead.
-        if r.URL.Path == "/hangman.html" {
-            filePath = "hangman.html"
-        }
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Default to serving index.html
+		filePath := "index.html"
 
-        if r.URL.Path == "/play.html" {
-            filePath = "play.html"
-        }
-        
-        http.ServeFile(w, r, filePath)
-    })
+		// If the path is explicitly /hangman.html, serve that file instead.
+		if r.URL.Path == "/hangman.html" {
+			filePath = "hangman.html"
+		}
+		if r.URL.Path == "/play.html" {
+			filePath = "play.html"
+		}
 
-    http.HandleFunc("/assets/style.css", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "assets/style.css")
-    })
+		http.ServeFile(w, r, filePath)
+	})
 
-    http.HandleFunc("/assets/hangmanstyle.css", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "assets/hangmanstyle.css")
-    })
+	http.HandleFunc("/assets/style.css", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "assets/style.css")
+	})
 
-    http.HandleFunc("/assets/playstyle.css", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "assets/playstyle.css")
-    })
+	http.HandleFunc("/assets/hangmanstyle.css", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "assets/hangmanstyle.css")
+	})
 
-    http.HandleFunc("/assets/script.js", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "assets/script.js")
-    })
+	http.HandleFunc("/assets/playstyle.css", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "assets/playstyle.css")
+	})
 
-    fmt.Println("Server is running on http://localhost:8080")
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/assets/script.js", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "assets/script.js")
+	})
+
+	http.HandleFunc("/play", func(w http.ResponseWriter, r *http.Request) {
+		method := r.Method
+
+		if method == "POST" {
+			r.ParseForm()
+			difficulty := r.Form.Get("difficulty")
+			fmt.Println("POST request successful")
+			fmt.Println("Form data: ", r.Form)
+			fmt.Println(difficulty)
+			template.Must(template.ParseFiles("play.html")).Execute(w, nil)
+		} else if method == "GET" {
+			fmt.Println("GET request successful")
+		}
+
+	})
+
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
